841341408044d02b4c1a566e76302277d9c8c739
TenzinBraun <tenzin.braun@gmail.com>
2024-06-05 14:32:29 +0200

    [HOTFIX] Api Key
    - Add return message when api key isn't valid or filled in

--- tmdb_app/lib/ui/screens/home/_movies_consumer_widget.dart
+++ tmdb_app/lib/ui/screens/home/_movies_consumer_widget.dart
@@ -1,12 +1,19 @@
 part of 'home_screen.dart';
 
-
 class MoviesConsumerWidget extends ConsumerWidget {
   const MoviesConsumerWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var movies = ref.watch(movieListProvider);
+    if (movies.isEmpty) {
+      return const Center(
+        child: Text(
+          "Oups... Couldn't load movies\nCheck if your API_KEY is valid or filled in",
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
     return CustomScrollView(
       slivers: [
         SliverList(
