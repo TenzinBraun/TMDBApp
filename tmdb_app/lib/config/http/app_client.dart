c8d209870a533961857d97720b546e5dcb6fa6dd
TenzinBraun <tenzin.braun@gmail.com>
2024-06-23 15:54:00 +0200

    [TEST] clean and test

--- tmdb_app/lib/config/http/app_client.dart
+++ tmdb_app/lib/config/http/app_client.dart
@@ -2,8 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppClient {
-  // static String securedApiKey = dotenv.env['API_KEY'] ?? 'Or Paste your API KEY here.';
-  static String securedApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OTZjN2VhYTAzMzViZDU2NGI4MWNlMmJhNWMzYjM5ZSIsInN1YiI6IjY2NTk5NTc1MTM1MDVkY2VjYzUwOGY5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mY-mxgg3oOwG1kc94dyO2jXF0Smd9RPP41vTKg31fwg";
+  static String securedApiKey = dotenv.env['API_KEY'] ?? 'Or Paste your API KEY here.';
   static String token = "Bearer $securedApiKey";
   static Map<String, String> headers = {
     "Content-Type": "application/json",
